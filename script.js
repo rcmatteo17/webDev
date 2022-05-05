0596a339fa6b75d2a49c9b3ecbcd98bd6e1cfd36
rcmatteo17 <89623529+rcmatteo17@users.noreply.github.com>
2022-05-05 15:38:42 +1000

    fix script

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const bar = document.getElementById('bar');
 const nav = document.getElementById('navbar');
 
 if (bar){
-    bar.addEventListener('click', () =>){
+    bar.addEventListener('click', () =>{
         nav.classList.add('active');
-    }
+    })
 }
