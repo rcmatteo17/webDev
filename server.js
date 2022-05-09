7f1e347c58c49041c0b3fb06a253e759bc285095
rcmatteo17 <89623529+rcmatteo17@users.noreply.github.com>
2022-05-09 20:40:11 +1000

    db

--- server.js
+++ server.js
@@ -6,10 +6,13 @@ const path = require('path');
 var admin = require("firebase-admin");
 
 let serviceAccount = require(".ecom-project1-firebase-adminsdk-ktbty-c6c2811f15.json");
+const { EnumValueDescriptorProto } = require('protobufjs/ext/descriptor');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
+
+let db = admin.firestore();
 
 let staticPath = path.join(__dirname,);
 
@@ -28,6 +31,27 @@ app.get(',signUp', (req, res) => {
 
 app.post('signUp', (req, res) => {
     console.log(req.body);
+
+    db.collection('users').doc(email).get()
+    .then(user => {
+        if(users.exists) {
+            return res.json({'alert': 'email already exists'});
+        }else{
+            bcrypt.genSalt(10, (err, salt) => {
+                bcrytp.hash(password, salt, (err, hash) => {
+                    req.body.password = hash;
+                    db.collection('users').doc(email).set(req.body)
+                    .then(data => {
+                        res.json({
+                            name: req.body.name,
+                            email: req.body.email,
+                        })
+                    })
+                })
+            })
+        }
+    })
+
     res.json('data recieved');
 })
 
