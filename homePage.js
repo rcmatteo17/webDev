ab964566419955aa8ad23258781cae8367375ea6
rcmatteo17 <89623529+rcmatteo17@users.noreply.github.com>
2022-05-09 13:44:02 +1000

    homepage.js and shop.js

---
+++ homePage.js
@@ -0,0 +1,28 @@
+const navbar = document.querySelector('.navbar');
+
+window.addEventListener('scroll', () => {
+    if(scrollY >+ 180){
+        navbar.classList.add('bg');
+    }else{
+           navbar.classList.remove('bg'); 
+    }
+    console.log(scrollY);
+} )
+
+const collageImages = [...document.querySelector('.collage-img')]
+
+collageImages.map((item, i) => {
+    item.addEventListener('mousover', () =>{
+        collageImages.map((image, index) => {
+            if (index != i){
+                image.style.filter = 'blur(10px)';
+                item.style.zIndex = 2;
+            }
+        })
+    })
+    item.addEventListener('mouseLeave', () => {
+        collageImages.map((image, index) => {
+            image.style = null;
+        }) 
+    })
+})
