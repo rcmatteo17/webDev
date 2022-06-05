2c9cd8b7ea2bf661067ee070506a815d42ac73d1
rcmatteo17 <89623529+rcmatteo17@users.noreply.github.com>
2022-06-06 06:48:29 +1000

    max login attempts implemented

--- login.php
+++ login.php
@@ -1,83 +1,57 @@
 <?php
-// Initialize the session
-session_start();
+	session_start();
  
-// Check if the user is already logged in, if yes then redirect him to welcome page
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-    header("location: homePage.php");
-    exit;
-}
+	if(isset($_POST['login'])){
+		//connection
+		$conn = new mysqli('localhost', 'root', '', 'dbase');
  
-// Include config file
-require_once "config.php";
+		//set login attempt if not set
+		if(!isset($_SESSION['attempt'])){
+			$_SESSION['attempt'] = 0;
+		}
  
-// Define variables and initialize with empty values
-$username = $password = "";
-$username_err = $password_err = $login_err = "";
+		//check if there are 3 attempts already
+		if($_SESSION['attempt'] == 3){
+			$_SESSION['error'] = 'Attempt limit reach';
+		}
+		else{
+			//get the user with the email
+			$sql = "SELECT * FROM users WHERE username = '".$_POST['username']."'";
+			$query = $conn->query($sql);
+			if($query->num_rows > 0){
+				$row = $query->fetch_assoc();
+				//verify password
+				if(password_verify($_POST['password'], $row['password'])){
+					//action after a successful login
+					//for now just message a successful login
+					$_SESSION['success'] = 'Login successful';
+					//unset our attempt
+					unset($_SESSION['attempt']);
+				}
+				else{
+					$_SESSION['error'] = 'Password incorrect';
+					//this is where we put our 3 attempt limit
+					$_SESSION['attempt'] += 1;
+					//set the time to allow login if third attempt is reach
+					if($_SESSION['attempt'] == 3){
+						$_SESSION['attempt_again'] = time() + (5*60);
+						//note 5*60 = 5mins, 60*60 = 1hr, to set to 2hrs change it to 2*60*60
+					}
+				}
+			}
+			else{
+				$_SESSION['error'] = 'No account with that username';
+			}
  
-// Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
+		}
  
-    // Check if username is empty
-    if(empty(trim($_POST["username"]))){
-        $username_err = "Please enter username.";
-    } else{
-        $username = trim($_POST["username"]);
-    }
-    
-    // Check if password is empty
-    if(empty(trim($_POST["password"]))){
-        $password_err = "Please enter your password.";
-    } else{
-        $password = trim($_POST["password"]);
-    }
-    
-    // Validate credentials
-    if(empty($username_err) && empty($password_err)){
-        // Prepare a select statement
-        $sql = "SELECT id, username, password FROM users WHERE username = ?";
-        
-        if($stmt = mysqli_prepare($link, $sql)){
-            // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "s", $param_username);
-            
-            // Set parameters
-            $param_username = $username;
-            
-            // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
-                // Store result
-                mysqli_stmt_store_result($stmt);
-                
-                // Check if username exists, if yes then verify password
-                if(mysqli_stmt_num_rows($stmt) == 1){                    
-                    // Bind result variables
-                    mysqli_stmt_bind_result($stmt, $id, $username, $hashed_password);
-                    if(mysqli_stmt_fetch($stmt)){
-                        if(password_verify($password, $hashed_password)){
-                            // Password is correct, so start a new session
-                            session_start();
-                            
-                            // Store data in session variables
-                            $_SESSION["loggedin"] = true;
-                            $_SESSION["id"] = $id;
-                            $_SESSION["username"] = $username;                            
-                            
-                            // Redirect user to welcome page
-                            header("location: homePage.php");
-                        } else{
-                            // Password is not valid, display a generic error message
-                            $login_err = "Invalid username or password.";
-                        }
-                    }
-                } else{
-                    // Username doesn't exist, display a generic error message
-                    $login_err = "Invalid username or password.";
-                }
-            } else{
-                echo "Oops! Something went wrong. Please try again later.";
-            }
-
+	}
+	else{
+		$_SESSION['error'] = 'Fill up login form first';
+	}
+ 
+	header('location: index.php');
+ 
             // Close statement
             mysqli_stmt_close($stmt);
         }
